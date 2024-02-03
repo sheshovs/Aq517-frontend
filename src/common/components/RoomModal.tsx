ef923f0f470f57ff0be84922af456f59163f792f
Sergio Vargas <sheshovs@gmail.com>
2024-02-03 17:15:57 -0300

    Add transition on modal

--- src/common/components/RoomModal.tsx
+++ src/common/components/RoomModal.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Fade,
   Grid,
   IconButton,
   Link,
@@ -41,6 +42,9 @@ const RoomModal = ({ roomInformation, roomPhotos, open, onClose }: RoomModalProp
   } = useTheme()
   const mobileWidth = useMediaQuery((theme: Theme) => theme.breakpoints.down(`md`))
   const [selectedPhotoIndex, setSelectedIndexPhoto] = React.useState(0)
+
+  //TODO: add modal skeleton
+
   return (
     <Modal
       open={open}
@@ -51,145 +55,145 @@ const RoomModal = ({ roomInformation, roomPhotos, open, onClose }: RoomModalProp
         justifyContent: `center`,
       }}
     >
-      <Grid
-        container
-        item
-        maxHeight="90vh"
-        lg={8}
-        sm={10}
-        xs={11}
-        padding={4}
-        bgcolor="main.white"
-        gap={2}
-        flexWrap="nowrap"
-        position="relative"
-        flexDirection={{ xs: `column`, md: `row` }}
-        overflow="auto"
-        sx={{
-          borderRadius: 2,
-          ':focus-visible': {
-            outline: `none`,
-          },
-        }}
-      >
-        <IconButton
+      <Fade in={open}>
+        <Grid
+          container
+          item
+          maxHeight="90vh"
+          lg={8}
+          sm={10}
+          xs={11}
+          padding={4}
+          bgcolor="main.white"
+          gap={2}
+          flexWrap="nowrap"
+          position="relative"
+          flexDirection={{ xs: `column`, md: `row` }}
+          overflow="auto"
           sx={{
-            position: `absolute`,
-            right: 10,
-            top: 10,
-            padding: 0.5,
-            borderRadius: 1,
+            borderRadius: 2,
+            ':focus-visible': {
+              outline: `none`,
+            },
           }}
-          onClick={onClose}
         >
-          <Icon icon="close" />
-        </IconButton>
-        <Grid container item xs={12} md={7} flexDirection="column" gap={2}>
-          <Grid container alignItems="center" gap={0.5}>
-            {!mobileWidth ? (
-              <IconButton
-                disabled={selectedPhotoIndex === 0}
-                sx={{
-                  paddingY: 2,
-                  paddingX: 0.5,
-                  borderRadius: 2,
-                }}
-                onClick={
-                  selectedPhotoIndex === 0
-                    ? () => null
-                    : () => setSelectedIndexPhoto(selectedPhotoIndex - 1)
-                }
-              >
-                <Icon icon="backArrow" />
-              </IconButton>
-            ) : null}
-
-            <Grid container item xs>
-              {roomPhotos?.[selectedPhotoIndex] ? (
-                <img
-                  src={roomPhotos?.[selectedPhotoIndex]}
-                  width="100%"
-                  height="100%"
-                  loading="lazy"
-                  style={{
-                    borderRadius: `4px`,
-                  }}
-                />
-              ) : (
-                <Skeleton
-                  animation="wave"
-                  variant="rounded"
-                  height={400}
-                  sx={{
-                    width: `100%`,
-                  }}
-                />
-              )}
-            </Grid>
-            {!mobileWidth ? (
-              <IconButton
-                disabled={selectedPhotoIndex === roomPhotos?.length - 1}
-                sx={{
-                  paddingY: 2,
-                  paddingX: 0.5,
-                  borderRadius: 2,
-                }}
-                onClick={
-                  selectedPhotoIndex === roomPhotos?.length - 1
-                    ? () => null
-                    : () => setSelectedIndexPhoto(selectedPhotoIndex + 1)
-                }
-              >
-                <Icon icon="nextArrow" />
-              </IconButton>
-            ) : null}
-          </Grid>
-          <Grid
-            container
-            gap={2}
-            justifyContent="flex-start"
-            paddingBottom={1}
-            alignItems="center"
-            flexWrap="nowrap"
+          <IconButton
             sx={{
-              overflowX: `auto`,
+              position: `absolute`,
+              right: 10,
+              top: 10,
+              padding: 0.5,
+              borderRadius: 1,
             }}
+            onClick={onClose}
           >
-            {roomPhotos?.map((photo, index) => (
-              <Image
-                key={index}
-                src={photo}
-                width={100}
-                height={80}
-                loading="lazy"
-                selectedPhotoIndex={selectedPhotoIndex}
-                index={index}
-                primary={primary}
-                onClick={() => setSelectedIndexPhoto(index)}
-              />
-            ))}
+            <Icon icon="close" />
+          </IconButton>
+          <Grid container item xs={12} md={7} flexDirection="column" gap={2}>
+            <Grid container alignItems="center" gap={0.5}>
+              {!mobileWidth ? (
+                <IconButton
+                  disabled={selectedPhotoIndex === 0}
+                  sx={{
+                    paddingY: 2,
+                    paddingX: 0.5,
+                    borderRadius: 2,
+                  }}
+                  onClick={
+                    selectedPhotoIndex === 0
+                      ? () => null
+                      : () => setSelectedIndexPhoto(selectedPhotoIndex - 1)
+                  }
+                >
+                  <Icon icon="backArrow" />
+                </IconButton>
+              ) : null}
+
+              <Grid container item xs>
+                {roomPhotos?.[selectedPhotoIndex] ? (
+                  <img
+                    src={roomPhotos?.[selectedPhotoIndex]}
+                    width="100%"
+                    height="100%"
+                    style={{
+                      borderRadius: `4px`,
+                    }}
+                  />
+                ) : (
+                  <Skeleton
+                    animation="wave"
+                    variant="rounded"
+                    height={400}
+                    sx={{
+                      width: `100%`,
+                    }}
+                  />
+                )}
+              </Grid>
+              {!mobileWidth ? (
+                <IconButton
+                  disabled={selectedPhotoIndex === roomPhotos?.length - 1}
+                  sx={{
+                    paddingY: 2,
+                    paddingX: 0.5,
+                    borderRadius: 2,
+                  }}
+                  onClick={
+                    selectedPhotoIndex === roomPhotos?.length - 1
+                      ? () => null
+                      : () => setSelectedIndexPhoto(selectedPhotoIndex + 1)
+                  }
+                >
+                  <Icon icon="nextArrow" />
+                </IconButton>
+              ) : null}
+            </Grid>
+            <Grid
+              container
+              gap={2}
+              justifyContent="flex-start"
+              paddingBottom={1}
+              alignItems="center"
+              flexWrap="nowrap"
+              sx={{
+                overflowX: `auto`,
+              }}
+            >
+              {roomPhotos?.map((photo, index) => (
+                <Image
+                  key={index}
+                  src={photo}
+                  width={100}
+                  height={80}
+                  selectedPhotoIndex={selectedPhotoIndex}
+                  index={index}
+                  primary={primary}
+                  onClick={() => setSelectedIndexPhoto(index)}
+                />
+              ))}
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} md={5} flexDirection="column" gap={2} height="100%">
+            <Typography variant="h2">Sala {roomInformation?.title}</Typography>
+            <Typography>{ReactHtmlParser(roomInformation?.description || ``)}</Typography>
+            <Grid container alignSelf="flex-end" justifyContent="flex-end">
+              <Link href="#reserve">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    width: `200px`,
+                    height: `42px`,
+                  }}
+                  onClick={onClose}
+                >
+                  Reservar ahora
+                </Button>
+              </Link>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid container item xs={12} md={5} flexDirection="column" gap={2} height="100%">
-          <Typography variant="h2">Sala {roomInformation?.title}</Typography>
-          <Typography>{ReactHtmlParser(roomInformation?.description || ``)}</Typography>
-          <Grid container alignSelf="flex-end" justifyContent="flex-end">
-            <Link href="#reserve">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                  width: `200px`,
-                  height: `42px`,
-                }}
-                onClick={onClose}
-              >
-                Reservar ahora
-              </Button>
-            </Link>
-          </Grid>
-        </Grid>
-      </Grid>
+      </Fade>
     </Modal>
   )
 }
