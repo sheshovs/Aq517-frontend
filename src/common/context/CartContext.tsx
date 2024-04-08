0c7e3c194893c687989111f1cae9f906eca34d04
Sergio Vargas <sheshovs@gmail.com>
2024-04-08 14:21:50 -0400

    Invalidate queries and delete from cart in CartProvider

--- src/common/context/CartContext.tsx
+++ src/common/context/CartContext.tsx
@@ -43,7 +43,7 @@ const CartProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
       onSuccess: (_, event) => {
         const selectedDate = dayjs(event.date).format(`YYYY-MM-DD`) || ``
         const room = event.room || ``
-        queryClient.invalidateQueries(API_QUERY_KEYS.getAllEvents(selectedDate, room.name))
+        queryClient.invalidateQueries(API_QUERY_KEYS.getAllEvents(selectedDate, room.uuid))
         deleteFromCart(event)
       },
     },
